aa95023eb2dde543af7fbe6305056df4a789898e
barrosen <barrosen@post.bgu.ac.il>
2022-12-16 11:15:44 +0200

    second commit

--- static/javascript/SignIn.js
+++ static/javascript/SignIn.js
@@ -1,22 +1,23 @@
 const myForm = document.querySelector('#form1');
-const myUser = document.querySelector('#email');
-const myPass = document.querySelector('#pass');
-const msg = document.querySelector('.msg');
 
 myForm.addEventListener('submit',onSubmit);
 
 function onSubmit (e) {
     console.log(Users);
+    const myUser = document.querySelector('#email').value;
+    const myPass = document.querySelector('#pass').value;
+    const msg = document.querySelector('.msg');
     e.preventDefault();
-    if(myUser.value === '' || myPass.value === '') {
+    if(myUser === '' || myPass === '') {
         msg.classList.add('error');
         msg.innerHTML = 'Please enter all fields';
-        setTimeout(() => window.location.href = '../views/SignIn.html',1500);
+        setTimeout(()=>{ msg.innerHTML="",msg.classList.remove('error')},2000)
+        return false
     }
     else {
         let x = 0;
         for (let i = 0; i < Users.length; i++) {
-            if (Users[i].getUsername() === myUser.value && Users[i].getPassword() === myPass.value)
+            if (Users[i].getUsername() === myUser && Users[i].getPassword() === myPass)
             {
                 msg.classList.add('valid');
                 msg.innerHTML = 'Connection Successfull';
@@ -25,9 +26,27 @@ function onSubmit (e) {
             }
         }
         if(x === 0) {
+            for (let i = 0; i < Users.length; i++)
+            {
+                if (Users[i].getUsername() === myUser)
+                {
+                    msg.classList.add('error');
+                    msg.innerHTML = 'Password Invalid';
+                    setTimeout(()=>{ msg.innerHTML="",msg.classList.remove('error')},2000)
+                    return false;
+                }
+                if (Users[i].getPassword() === myPass)
+                {
+                    msg.classList.add('error');
+                    msg.innerHTML = 'Username Invalid';
+                    setTimeout(()=>{ msg.innerHTML="",msg.classList.remove('error')},2000)
+                    return false;
+                }
+            }
             msg.classList.add('error');
-            msg.innerHTML = 'Username or Password Invalid';
-            setTimeout(() =>window.location.href = '../views/SignIn.html', 1500);
+            msg.innerHTML = 'Both Username and Password Invalid';
+            setTimeout(()=>{ msg.innerHTML="",msg.classList.remove('error')},2000)
+            return false;
         }
     }
 }
--- static/javascript/SignUp.js
+++ static/javascript/SignUp.js
@@ -1,38 +1,83 @@
 const myForm = document.querySelector('#form2');
-const myMail = document.querySelector('#email');
-const myPass = document.querySelector('#pass');
-const myFirst = document.querySelector('#first');
-const myLast = document.querySelector('#last');
-const myAge = document.querySelector('#age')
-const msg2 = document.querySelector('.msg');
 
+myForm.addEventListener('submit', onSub);
 
-myForm.addEventListener('submit',onSub);
 function onSub(e) {
     e.preventDefault();
-    let x = 0;
+    const myMail = document.querySelector('#email').value;
+    const myPass = document.querySelector('#pass').value;
+    const myFirst = document.querySelector('#first').value;
+    const myLast = document.querySelector('#last').value;
+    const myAge = document.querySelector('#Age').value;
+    const msg = document.querySelector('.msg');
+    let letters = /^[A-Za-z]+$/;
+
     for (let i = 0; i < Users.length; i++) {
-        if (Users[i].getUsername() === myMail.value) {
-            msg2.classList.add('error');
-            msg2.innerHTML = 'Username in use';
-            setTimeout(() => window.location.href = '../views/SignUp.html', 1500);
-            x = 1
+        if (Users[i].getUsername() === myMail) {
+            msg.classList.add('error');
+            msg.innerHTML = 'Username in use';
+            setTimeout(()=>{ msg.innerHTML="",msg.classList.remove('error')},2000)
+            return false;
         }
     }
-        if(x==0) {
-            let letters = /^[A-Za-z]+$/;
-            if (myFirst.value.match(letters) && myLast.value.match(letters)) {
-                console.log(myMail.value);
-                var newUser = new User('myMail.value', 'myPass.value', 'myFirst.value', 'myLast.value', 'myAge.value');
-                Users.push(newUser);
-                console.log(Users);
-                msg2.classList.add('valid');
-                msg2.innerHTML = 'Account Created Succesfully';
-                setTimeout(() => window.location.href = '../views/SignIn.html', 1000);
-            } else {
-                msg2.classList.add('error');
-                msg2.innerHTML = 'Please Enter Only Letters in Name Fields';
-                setTimeout(() => window.location.href = '../views/SignUp.html', 1500);
-            }
-        }
+
+    if (EmailValidation(myMail) === false) {
+        msg.classList.add('error');
+        msg.innerHTML = "Invalid email address";
+        setTimeout(()=>{ msg.innerHTML="",msg.classList.remove('error')},2000)
+        return false;
     }
+
+    if (myPass.length < 8 && myPass.length < 15) {
+        msg.classList.add('error');
+        msg.innerHTML = 'Password must be at least 8 characters and less then 15';
+        setTimeout(()=>{ msg.innerHTML="",msg.classList.remove('error')},2000)
+        return false;
+    }
+
+    if (myLast.length<2 || myFirst.length<2)
+    {
+        msg.classList.add('error');
+        msg.innerHTML = "First and Last name must be at least 2 letters";
+        setTimeout(()=>{ msg.innerHTML="",msg.classList.remove('error')},2000)
+        return false;
+    }
+
+    if(myAge<15 || myAge>120)
+    {
+        msg.classList.add('error');
+        msg.innerHTML = "Minimum Age 15 and Maximum 120";
+        setTimeout(()=>{ msg.innerHTML="",msg.classList.remove('error')},2000)
+        return false;
+    }
+    if (myLast.match(letters) && myFirst.match(letters))
+    {
+        var newUser = new User('myMail', 'myPass', 'myFirst', 'myLast', 'myAge.value');
+        Users.push(newUser);
+        console.log(Users);
+        msg.classList.add('valid');
+        msg.innerHTML = 'Account Created Succesfully';
+        setTimeout(() => window.location.href = '../views/SignIn.html', 1000);
+    }
+    else {
+        msg.classList.add('error');
+        msg.innerHTML = "First and Last name must be english letters only";
+        setTimeout(()=>{ msg.innerHTML="",msg.classList.remove('error')},2000)
+        return false;
+    }
+}
+function EmailValidation(mail) {
+    if (/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(mail)) //regex to check if valid email
+    {
+        return true;
+    }
+    return false;
+}
+
+function PasswordValidation(password) {
+    if ((/^[a-zA-Z]$/) && (/^[a-zA-Z0-9]$/) && (/^[a-zA-Z0-9]*$/)) //regex to check if valid password
+    {
+        return true;
+    }
+    return false;
+}
